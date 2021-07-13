0cef87032a7ea14cd792e8c0d3eeb47643bade9d
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-07-13 23:13:02 +0300

    1. Create a file "flipBigCertificate.js" and include it in the file "index.html"; 2. Add the function of switching to the next and previous large certificates; 3. Add "cursor" when hovering over the pictures of certificates and when hovering over the buttons for managing a large certificate; 4. Add minor cosmetic improvements; 5. Add minor changes to the code.

--- scripts/bigCertificate.js
+++ scripts/bigCertificate.js
@@ -1,26 +1,31 @@
-function enlargeImg(event) {
+var j;
+var i;
+var lang;
+
+function enlargeImg(event, btnJ, btnI, btnLang) {
     var divMyCertificatesBig = document.querySelector('.my_certificates__big');
     var divMyCertificatesBigSpec = document.querySelector('.my_certificates__big__spec');
     divMyCertificatesBigSpec.innerHTML = '';
 
-    var jClick = this.id.split('_')[0];
-    var iClick = this.id.split('_')[1];
-    var lang = this.id.split('_')[2];
-    var j = 0, i = 0;
-
-    for (var j0 = 1; j0 <= myPartClass.length; j0++) {
-        var countCertificatesInHtml0 = masCountCertificatesInHtml[j0 - 1];
-        var myCertificates0 = masMyCertificates[j0 - 1];
-        for (var i0 = countCertificatesInHtml0 + 1; i0 <= myCertificates0.length + countCertificatesInHtml0; i0++) {
-            if (j0 == jClick && i0 == iClick) {
-                j = j0;
-                i = i0;
-                console.log('Финал', j, i, lang);
-
-                break;
-            }
-        }
+    if (btnI == undefined && btnJ == undefined && btnLang == undefined) {
+        j = this.id.split('_')[0];
+        i = this.id.split('_')[1];
+        lang = this.id.split('_')[2];
     }
+    else {
+        j = btnJ;
+        i = btnI;
+        lang = btnLang;
+    }
+
+    var btnBack = document.querySelector('.my_certificates__big__btn-back');
+    var btnNext = document.querySelector('.my_certificates__big__btn-next');
+
+    btnBack.setAttribute('id', j + '_' + i + '_' + lang + '_btnBack');
+    btnNext.setAttribute('id', j + '_' + i + '_' + lang + '_btnNext');
+
+    btnBack.onclick = imgBack;
+    btnNext.onclick = imgNext;
 
     console.log('Финал', j, i, lang);
 
@@ -30,10 +35,6 @@ function enlargeImg(event) {
         inputMyCertificatesBigSpecCheckbox.setAttribute('type', 'checkbox');
         inputMyCertificatesBigSpecCheckbox.setAttribute('id', 'certificate_big');
 
-        // if (lang == 'ru') {
-        //     console.log('Сработал none');
-        //     inputMyCertificatesBigSpecCheckbox.setAttribute('checked', 'none');
-        // }
         if (lang == 'en') {
             console.log('Сработал checked');
             inputMyCertificatesBigSpecCheckbox.setAttribute('checked', 'checked');
@@ -81,98 +82,12 @@ function enlargeImg(event) {
         divMyCertificatesBigSpec.append(labelMyCertificatesBigSpecLabel);
     }
 
+    if (!(masMyCertificates[j - 1][i - countCertificatesInHtml - 1].en == '') && !(masMyCertificates[j - 1][i - countCertificatesInHtml - 1].ru == '')) {
+        divMyCertificatesBigSpec.style.paddingBottom = '3px'
+    }
+    else {
+        divMyCertificatesBigSpec.style.paddingBottom = '0'
+    }
 
     divMyCertificatesBig.style.display = 'block';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var body = document.querySelector('body');
-
-// var p = document.createElement('p');
-// p.innerHTML = '3. *Добавить в галерею функцию перехода к следующему изображению. По сторонам от большой картинки должны быть стрелки «вперед» и «назад», по нажатию на которые происходит замена изображения на следующее или предыдущее.';
-// p.style.fontWeight = 'bold';
-// body.append(p);
-
-// var divImgBig2 = document.createElement('div');
-// var divImgSmall2 = document.createElement('div');
-// var divButton = document.createElement('div');
-// var products = ['cheese', 'sausage', 'bread'];
-
-
-
-// function enlargeImg(event) {
-//     var imgBig = document.createElement('img');
-//     imgBig.width = '300';
-//     imgBigSrc = event.target.src;
-//     imgBig.src = imgBigSrc;
-//     var parts1 = imgBigSrc.split('/');
-//     var parts2 = parts1[parts1.length - 1].split('.');
-//     imgBig.alt = parts2[0];
-//     divImgBig2.innerHTML = '';
-//     divImgBig2.append(imgBig);
-//     var i = 0;
-//     for (var product of products) {
-//         if (product == imgBig.alt) {
-//             certificateIndex = i;
-//             break;
-//         }
-//         i++;
-//     }
-// }
-
-// var certificateIndex = 0;
-
-// function imgBigParam() {
-//     var imgBig = document.createElement('img');
-//     imgBig.width = '300';
-//     imgBigSrc = 'img/small/' + products[certificateIndex] + '.jpg'
-//     imgBig.src = imgBigSrc;
-//     var parts1 = imgBigSrc.split('/');
-//     var parts2 = parts1[parts1.length - 1].split('.');
-//     imgBig.alt = parts2[0];
-//     divImgBig2.append(imgBig);
-// }
-
-// imgBigParam();
-
-// function imgBack() {
-//     (certificateIndex == 0) ? certificateIndex = products.length - 1 : certificateIndex--;
-//     divImgBig2.innerHTML = '';
-//     imgBigParam();
-// }
-
-// function imgNext() {
-//     (certificateIndex == products.length - 1) ? certificateIndex = 0 : certificateIndex++;
-//     divImgBig2.innerHTML = '';
-//     imgBigParam();
-// }
-
-// var buttonBack = document.createElement('button');
-// var buttonNext = document.createElement('button');
-// buttonBack.innerText = 'Назад';
-// buttonNext.innerText = 'Вперёд';
-// buttonBack.onclick = imgBack;
-// buttonNext.onclick = imgNext;
-// divButton.append(buttonBack);
-// divButton.append(buttonNext);
-
-// body.append(divButton);
-// body.append(divImgSmall2);
-// body.append(divImgBig2);

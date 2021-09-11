efc31a284a616a2ae66bcb41a83fa2c68db75d12
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-09-11 16:26:17 +0300

    1. Add a new field to the list of projects (change only the js-code); 2. Add one more project to the list of projects (change only js-code); 3. Add html-code for projects (change vue.js-code and scss-code); 4. Add a referral link on the "GeekBrains" (change the html-code and scss-code); 5. Add animation for the "footer" section (add only scss-code); 6. Add some small cosmetic changes (change html-code and scss-code).

--- scripts/vueComponentMyProjects.js
+++ scripts/vueComponentMyProjects.js
@@ -3,64 +3,81 @@ const allProjects = [
         category: 'Учебный проект',
         title: 'New Automation',
         hrefSite: 'https://juris-berkulis.github.io/GB_0902/',
-        technologies: 'HTML, SCSS',
-        hrefGitHub: 'https://juris-berkulis.github.io/GB_0902/'
+        technologies: 'HTML, SCSS, Figma',
+        hrefGitHub: 'https://juris-berkulis.github.io/GB_0902/',
+        place: 'GitHub'
     },
     {
         category: 'Учебный проект',
         title: 'Simple',
         hrefSite: 'https://juris-berkulis.github.io/GB_1204/',
-        technologies: 'HTML, SCSS',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1204'
+        technologies: 'HTML, SCSS, Figma',
+        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1204',
+        place: 'GitHub'
     },
     {
         category: 'Учебный проект',
         title: 'Waxom',
         hrefSite: 'https://juris-berkulis.github.io/GB_1204_Second_Layout/',
-        technologies: 'HTML, SCSS',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1204_Second_Layout'
+        technologies: 'HTML, SCSS, Figma',
+        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1204_Second_Layout',
+        place: 'GitHub'
     },
     {
-        category: 'Собственная игра',
+        category: 'Полностью собственная игра',
         title: 'Duck hunt',
         hrefSite: 'https://juris-berkulis.github.io/Duck_hunt/',
         technologies: 'HTML, SCSS',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/Duck_hunt'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/Duck_hunt',
+        place: 'GitHub'
     },
     {
         category: 'Тренировочный проект',
         title: 'Burger menu',
         hrefSite: 'https://juris-berkulis.github.io/Burger_menu/by_css_with_transition.html',
         technologies: 'HTML, SCSS, JavaScript',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/Burger_menu'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/Burger_menu',
+        place: 'GitHub'
     },
     {
         category: 'Учебный проект',
         title: 'Home Work. JS - Level 1',
         hrefSite: 'https://juris-berkulis.github.io/GB_0106_JS_L1_HW/',
         technologies: 'JavaScript на ES5, DOM',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_0106_JS_L1_HW'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_0106_JS_L1_HW',
+        place: 'GitHub'
     },
     {
-        category: 'Собственный сайт',
-        title: 'Этот сайт',
+        category: 'Полностью собственный сайт',
+        title: 'Этот сайт-резюме',
         hrefSite: '',
         technologies: 'HTML, SCSS, JavaScript на ES5, DOM, JavaScript на ES6, Vue.js',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/Res_JB'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/Res_JB',
+        place: 'GitHub'
+    },
+    {
+        category: 'Учебный проект',
+        title: 'Кухонная Фея',
+        hrefSite: 'http://kitchen.fairy.by.olivier.tilda.ws/',
+        technologies: 'Инструменты Tilda',
+        hrefGitHub: '',
+        place: 'Tilda'
     },
     {
         category: 'Учебный проект',
         title: 'Интернет-магазин с корзиной',
         hrefSite: 'https://juris-berkulis.github.io/04_JavaScript_Level_2_Site_with_only_Frontend/',
         technologies: 'HTML, SCSS, JavaScript на ES6, внешний API, Vue.js',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/04_JavaScript_Level_2_Site_with_only_Frontend'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/04_JavaScript_Level_2_Site_with_only_Frontend',
+        place: 'GitHub'
     },
     {
         category: 'Составной учебный проект',
         title: 'Интернет-магазин с корзиной',
         hrefSite: '',
         technologies: 'HTML, SCSS, JavaScript на ES6, внешний API, JSON, Vue.js, сторонний Node.js, WebPack',
-        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1907_JS_L2_HW'
+        hrefGitHub: 'https://github.com/Juris-Berkulis/GB_1907_JS_L2_HW',
+        place: 'GitHub'
     }
 ]
 
@@ -78,7 +95,8 @@ const myprojectitem = {
                 </div>
                 <p class="my_projects__cont__projects__item__technologies">{{ projectItem.technologies }}</p>
                 <p class="my_projects__cont__projects__item__GitHub">Проект на 
-                    <a class="my_projects__cont__projects__item__GitHub__link" target="_blank" :href="projectItem.hrefGitHub">GitHub</a>
+                    <a class="my_projects__cont__projects__item__GitHub__link" target="_blank" :href="projectItem.hrefGitHub" v-if="projectItem.hrefGitHub">{{ projectItem.place }}</a>
+                    <span class="my_projects__cont__projects__item__GitHub__spare" v-else="projectItem.hrefGitHub">{{ projectItem.place }}</span>
                 </p>
             </div>
             `
